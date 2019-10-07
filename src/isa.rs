27343a94b73febd8a00ed8500e11d1c148b3427d
Rahul Butani <rr.butani@gmail.com>
2019-10-07 02:34:16 -0500

    clean up isa, a bit

--- src/isa.rs
+++ src/isa.rs
@@ -1,5 +1,5 @@
-use super::{Word};
-use core::ops::RangeInclusive;
+use super::Word;
+use core::ops::Range;
 
 pub type RegNum = u8;
 
@@ -27,60 +27,84 @@ pub enum Instruction {
     Trap { trapvec: u8 },
 }
 
-trait Bits {
+trait Bits: Sized {
     fn bit(self, bit: u32) -> bool;
 
-    fn bits(self, range: RangeInclusive<u32>) -> Self;
+    fn b(self, bit: u32) -> bool {
+        self.bit(bit)
+    }
+
+    fn bits(self, range: Range<u32>) -> usize;
+
+    fn u8(self, range: Range<u32>) -> u8 {
+        assert!(range.end - range.start <= 8);
+        self.bits(range) as u8
+    }
+
+    fn u16(self, range: Range<u32>) -> u16 {
+        assert!(range.end - range.start <= 16);
+        self.bits(range) as u16
+    }
+
+    fn u32(self, range: Range<u32>) -> u32 {
+        assert!(range.end - range.start <= 32);
+        self.bits(range) as u32
+    }
 }
 
-impl Bits for u16 {
+impl Bits for Word {
     fn bit(self, bit: u32) -> bool {
         ((self >> bit) & 1) == 1
     }
 
-    fn bits(self, range: RangeInclusive<u32>) -> u16 {
-        let mask = !(core::u16::MAX << (range.end() - range.start()));
-        (self >> range.start()) & mask
+    fn bits(self, range: Range<u32>) -> usize {
+        let mask = !(core::u16::MAX << (range.end - range.start));
+        ((self >> range.start) & mask) as usize
     }
 }
 
 impl From<Word> for Instruction {
-
     // Assuming Word = u16; compile error if not.
     fn from(w: u16) -> Self {
         use Instruction::*;
 
         match w >> 12 {
-            0b0000 => Br { n: w.bit(11), z: w.bit(10), p: w.bit(9), offset9: w.bits(0..=8) },
-            0b0001 => match w.bit(5) {
-                false => AddReg { dr: w.bits(9..=11) as u8, sr1: w.bits(6..=8) as u8, sr2: w.bits(0..=2) as u8 },
-                true => AddImm { dr: w.bits(9..=11) as u8, sr1: w.bits(6..=8) as u8, imm5: w.bits(0..=4) as u8 },
+            0b0000 => Br { n: w.b(11), z: w.b(10), p: w.b(9), offset9: w.u16(0..8) },
+            0b0001 => match w.b(5) {
+                false => AddReg { dr: w.u8(9..11), sr1: w.u8(6..8), sr2: w.u8(0..2) },
+                true => AddImm { dr: w.u8(9..11), sr1: w.u8(6..8), imm5: w.u8(0..4) },
             },
-            0b0010 => Ld { dr: w.bits(9..=11) as u8, offset9: w.bits(0..=8) },
-            0b0011 => St { sr: w.bits(9..=11) as u8, offset9: w.bits(0..=8) },
+            0b0010 => Ld { dr: w.u8(9..11), offset9: w.u16(0..8) },
+            0b0011 => St { sr: w.u8(9..11), offset9: w.u16(0..8) },
             0b0100 => match w.bit(11) {
-                true => Jsr { offset11: w.bits(0..=10) },
-                false => Jsrr { base: w.bits(6..=8) as u8 },
+                true => Jsr { offset11: w.u16(0..10) },
+                false => Jsrr { base: w.u8(6..8) },
             },
             0b0101 => match w.bit(5) {
-                false => AndReg { dr: w.bits(9..=11) as u8, sr1: w.bits(6..=8) as u8, sr2: w.bits(0..=2) as u8 },
-                true => AndImm { dr: w.bits(9..=11) as u8, sr1: w.bits(6..=8) as u8, imm5: w.bits(0..=4) as u8 },
+                false => AndReg { dr: w.u8(9..11), sr1: w.u8(6..8), sr2: w.u8(0..2) },
+                true => AndImm {  dr: w.u8(9..11), sr1: w.u8(6..8), imm5: w.u8(0..4) },
             },
-            0b0110 => Ldr { dr: w.bits(9..=11) as u8, base: w.bits(6..=8) as u8, offset6: w.bits(0..=5) as u8 },
-            0b0111 => Str { dr: w.bits(9..=11) as u8, base: w.bits(6..=8) as u8, offset6: w.bits(0..=5) as u8 },
+            0b0110 => Ldr { dr: w.u8(9..11), base: w.u8(6..8), offset6: w.u8(0..5) },
+            0b0111 => Str { dr: w.u8(9..11), base: w.u8(6..8), offset6: w.u8(0..5) },
             0b1000 => Rti,
-            0b1001 => Not { dr: w.bits(9..=11) as u8, sr: w.bits(6..=8) as u8 },
-            0b1010 => Ldi { dr: w.bits(9..=11) as u8, offset9: w.bits(0..=8) },
-            0b1011 => Sti { sr: w.bits(9..=11) as u8, offset9: w.bits(0..=8) },
-            0b1100 => match w.bits(6..=8) {
-                reg @ 0..=6 => Jmp { base: reg as u8 },
+            0b1001 => Not { dr: w.u8(9..11), sr: w.u8(6..8) },
+            0b1010 => Ldi { dr: w.u8(9..11), offset9: w.u16(0..8) },
+            0b1011 => Sti { sr: w.u8(9..11), offset9: w.u16(0..8) },
+            0b1100 => match w.u8(6..8) {
+                base @ 0..=6 => Jmp { base },
                 7 => Ret,
                 _ => unreachable!(),
             },
             0b1101 => unimplemented!(),
-            0b1110 => Lea { dr: w.bits(9..=11) as u8, offset9: w.bits(0..=8) },
-            0b1111 => Trap { trapvec: w.bits(0..=7) as u8 },
+            0b1110 => Lea { dr: w.u8(9..11), offset9: w.u16(0..8) },
+            0b1111 => Trap { trapvec: w.u8(0..7) },
             _ => unreachable!(),
         }
+    }
+}
+
+impl From<Instruction> for Word {
+    fn from(ins: Instruction) -> u16 {
+
     }
 }
