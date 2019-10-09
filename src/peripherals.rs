8c4200c6eeba41ef8e0d0a4f823c7241e0ee8b1c
Rahul Butani <rr.butani@gmail.com>
2019-10-09 03:13:03 -0500

    peripherals: switch to using enums for gpio pins

--- src/peripherals.rs
+++ src/peripherals.rs
@@ -1,14 +1,38 @@
-use super::{Addr, Word};
+use super::Word;
 
 trait Peripherals: Gpio + Adc + Pwm + Timers {
     fn generic_init(&mut self);
 }
 
-pub const NUM_GPIO_PINS: u8 = 8; // G0 - G7
-pub enum GpioState { Input, Output, Interrupt, Disabled }
+// Switched to using enums to identify peripheral pin numbers; this way
+// referring to invalid/non-existent pin numbers isn't an error that peripheral
+// trait implementations have to deal with.
+//
+// This seems to make more since, if you consider that the peripherals are
+// exposed through a memory-mapped interface an invalid pin number isn't really
+// an error that can happen (you either hit a memory address that corresponds
+// to a peripheral or you hit an invalid memory address).
+//
+// This is currently a little wonky, but it'll be better once we write the macro
+// described in `control.rs`.
+pub enum GpioPin { G0, G1, G2, G3, G4, G5, G6, G7 }
+pub const NUM_GPIO_PINS: u8 = 8; // G0 - G7; TODO: derive macro (also get it to impl Display)
+const GPIO_PINS: [GpioPin; NUM_GPIO_PINS as usize] = {
+    use GpioPin::*;
+    [G0, G1, G2, G3, G4, G5, G6, G7]
+}; // TODO: once we get the derive macro, get rid of this.
+
+pub enum GpioState {
+    Input,
+    Output,
+    Interrupt, // TBD: Can you call read on a pin configured for interrupts?
+    Disabled,
+}
 pub trait Gpio {
-    fn change_state(&mut self, pin: u8, state: GpioState) -> Result<(), ()>;
-    fn get_state(&self, pin: u8) -> Option<GpioState>;
+    fn set_state(&mut self, pin: GpioPin, state: GpioState) -> Result<(), GpioMiscError>; // should probably be infallible
+    fn get_state(&self, pin: GpioPin) -> GpioState;
+    fn read(&self, pin: GpioPin) -> Result<bool, GpioReadError>; // errors on state mismatch (i.e. you tried to read but the pin is configured as an output)
+    fn write(&mut self, pin: GpioPin, bit: bool) -> Result<(), GpioWriteError>; // errors on state mismatch
 
     fn read(&self, pin: u8) -> Result<bool, ()>;
     fn read_all(&self, pin: u8) -> Result<[bool; NUM_GPIO_PINS as usize], ()>;
