657470d20a86010fdc07ba68b399862818443f09
Rahul Butani <rr.butani@gmail.com>
2019-10-09 18:01:44 -0500

    shims: now separate

---
+++ src/shims/memory.rs
@@ -0,0 +1,59 @@
+//! An extremely naive, terrible [`Memory` trait](../memory/trait.Memory.html)
+//! implementation called [`MemoryShim`](struct.MemoryShim.html).
+
+/// Naive [`Memory` trait](../memory/trait.Memory.html) implementation.
+///
+/// Only good for hosted platforms since we just go and use 128 KiB of stack
+/// space.
+pub struct MemoryShim {
+    persistent: [Word; ((core::u16::MAX) / 2) as usize],
+    staging: [Word; ((core::u16::MAX) / 2) as usize],
+}
+
+impl Default for MemoryShim {
+    fn default() -> Self {
+        Self {
+            persistent: [0u16; ((core::u16::MAX) / 2) as usize],
+            staging: [0u16; ((core::u16::MAX) / 2) as usize]
+        }
+    }
+}
+
+// TODO: move all shims to a module called shims
+// TODO: feature gate the shims
+// TODO: split gpio, adc, etc. into separate files in a peripherals module
+
+impl MemoryShim {
+    fn new(memory: [Word; ((core::u16::MAX) / 2) as usize]) -> Self {
+        Self {
+            persistent: memory,
+            staging: memory.clone()
+        }
+    }
+
+    fn dump_to_file() {
+        unimplemented!()
+        // TODO!
+    }
+
+    fn from_file() {
+        unimplemented!()
+        // TODO!
+    }
+}
+
+impl Memory for MemoryShim {
+    fn read_word(&self, addr: Addr) -> Word {
+        self.staging[addr as usize]
+    }
+
+    fn write_word(&mut self, addr: Addr, word: Word) {
+        self.staging[addr as usize] = word;
+    }
+
+    fn commit(&mut self) -> Result<(), MemoryMiscError> {
+        self.persistent = self.staging.clone();
+
+        Ok(())
+    }
+}
---
+++ src/shims/mod.rs
@@ -0,0 +1,10 @@
+//! Shims for the various [peripheral](../peripheral) traits and for the
+//! [`Memory` trait](../memory/trait.Memory.html).
+
+// Peripherals:
+mod gpio;
+
+
+// Devices:
+
+// Memory:
