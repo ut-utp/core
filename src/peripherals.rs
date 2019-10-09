a004906960e15f6e9c67fe4835ae47a00114e4b1
Rahul Butani <rr.butani@gmail.com>
2019-10-09 03:15:07 -0500

    peripherals: notes for the rest of them

--- src/peripherals.rs
+++ src/peripherals.rs
@@ -157,34 +157,56 @@ impl TryFrom<GpioPinArr<Result<(), GpioWriteError>>> for GpioWriteErrors {
     }
 }
 
+// TODO: Switch to enum for pins
+// TODO: Add Errors
 pub const NUM_ADC_PINS: u8 = 4; // A0 - A3
-pub enum AdcState { Enabled, Disabled, Interrupt }
+pub enum AdcState {
+    Enabled,
+    Disabled,
+    Interrupt,
+}
 trait Adc {
-    fn change_state(&mut self, pin: u8, state: AdcState) -> Result<(), ()>;
+    fn set_state(&mut self, pin: u8, state: AdcState) -> Result<(), ()>;
     fn get_state(&self, pin: u8) -> Option<AdcState>;
+    // fn get_states() // TODO
+
 
     fn read(&self, pin: u8) -> Result<u8, ()>;
 
     fn register_interrupt(&mut self, pin: u8, func: impl FnMut(u8)) -> Result<(), ()>;
 }
 
-pub const NUM_PWM_PINS: u8 = 2; // P0 - A1
+// TODO: Switch to enum for pins
+// TODO: Add Errors
+pub const NUM_PWM_PINS: u8 = 2; // P0 - P1
+pub enum PwmState {
+    Enabled,
+    Disabled,
+}
 trait Pwm {
 // enable, disable, set duty cycle, enable hystersis. start
-    fn enable(&mut self, pin: u8);
+    fn set_state(&mut self, pin: u8, state: PwmState) -> Result<(), ()>;
+    fn get_state(&self, pin: u8) -> Option<PwmState>;
+    // fn get_states() // TODO
+
     fn set_duty_cycle(duty: u16);
     //Optionally enable hysterisis ?
     fn start(&mut self, pin: u8); //Start the periodic timer interrupt
     fn disable(&mut self, pin: u8);
-
 }
 
-pub const NUM_TIMERS: u8 = 2; // T0 - T1
-pub enum TimerState { Repeated, SingleShot, Disabled }
 
+// TODO: Switch to enum for pins
+// TODO: Add Errors
 // Timer periods: [0, core::u16::MAX)
+pub const NUM_TIMERS: u8 = 2; // T0 - T1
+pub enum TimerState {
+    Repeated,
+    SingleShot,
+    Disabled,
+}
 trait Timers {
-    fn change_state(&mut self, num: u8, state: TimerState) -> Result<(), ()>;
+    fn set_state(&mut self, num: u8, state: TimerState) -> Result<(), ()>;
     fn get_state(&mut self, num: u8) -> Option<TimerState>;
 
     fn set_period(&mut self, num: u8, milliseconds: Word);
@@ -195,8 +217,8 @@ trait Timers {
 
 // Just 1 Clock! (millisecond units)
 trait Clock {
-    fn enable(&mut self);
-    fn disable(&mut self);
+    // fn enable(&mut self);   // Probably ditch (TODO).
+    // fn disable(&mut self);
 
     fn get_milliseconds(&self) -> Word;
     fn set_milliseconds(&mut self, ms: Word);
