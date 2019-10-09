4a2f3bf58b4917fa3d7c60517922a808ff62a673
Rahul Butani <rr.butani@gmail.com>
2019-10-09 03:09:02 -0500

    memory: change the interface for opt-in persistence
    Changes only survive reboot if the LC-3 program explicitly requests that they do, it what we've settled on.

--- src/memory.rs
+++ src/memory.rs
@@ -4,29 +4,59 @@ pub trait Memory {
     fn read_word(&self, addr: Addr) -> Word;
     fn write_word(&mut self, addr: Addr, word: Word);
 
-    fn flush(&self) -> Result<(), ()>;
+    fn commit(&self) -> Result<(), MemoryMiscError>;
 }
 
+/// An extremely naive, terrible [`Memory`](trait.Memory.html) implementation.
 pub struct MemoryShim {
-    memory: [Word; ((core::u16::MAX) / 2) as usize],
+    persistent: [Word; ((core::u16::MAX) / 2) as usize],
+    staging: [Word; ((core::u16::MAX) / 2) as usize],
 }
 
 impl Default for MemoryShim {
     fn default() -> Self {
         Self {
-            memory: [0u16; ((core::u16::MAX) / 2) as usize]
+            persistent: [0u16; ((core::u16::MAX) / 2) as usize],
+            staging: [0u16; ((core::u16::MAX) / 2) as usize]
         }
+    }
+}
+
+// TODO: move all shims to a module called shims
+// TODO: feature gate the shims
+// TODO: split gpio, adc, etc. into separate files in a peripherals module
+
+impl MemoryShim {
+    fn new(memory: [Word; ((core::u16::MAX) / 2) as usize]) -> Self {
+        Self {
+            persistent: memory,
+            staging: memory.clone()
+        }
+    }
+
+    fn dump_to_file() {
+        unimplemented!()
+        // TODO!
+        }
+
+    fn from_file() {
+        unimplemented!()
+        // TODO!
     }
 }
 
 impl Memory for MemoryShim {
     fn read_word(&self, addr: Addr) -> Word {
-        self.memory[addr as usize]
+        self.staging[addr as usize]
     }
 
     fn write_word(&mut self, addr: Addr, word: Word) {
-        self.memory[addr as usize] = word;
+        self.staging[addr as usize] = word;
     }
 
-    fn flush(&self) -> Result<(), ()> { Ok(()) }
+    fn commit(&self) -> Result<(), MemoryMiscError> {
+        self.persistent = self.staging.clone();
+
+        Ok(())
+    }
 }
