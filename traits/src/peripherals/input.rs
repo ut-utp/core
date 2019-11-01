ef3de3891325a520a9e5c0401970d9d02b780e26
rrbutani <rr.butani@gmail.com>
2019-11-01 05:52:11 -0500

    traits: input is not simple or complete

--- traits/src/peripherals/input.rs
+++ traits/src/peripherals/input.rs
@@ -5,9 +5,28 @@ peripheral_trait! {input,
 pub trait Input: Default {
     /// Read a single ASCII character.
     /// Returns Err if the read fails.
-    fn read(&mut self) -> Result<u8, ReadError>;
-    
+    fn read(&mut self) -> Result<u8, ReadError>; // TODO: I think this should maybe go away..
+
+    // fn register_interrupt(&mut self, ) // TODO!!!
 }}
 
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct ReadError;
+
+// TODO: roll this into the macro
+using_std! {
+    use std::sync::{Arc, RwLock};
+    impl<I: Input> Input for Arc<RwLock<I>> {
+        fn read(&mut self) -> Result<u8, ReadError> {
+            RwLock::write(self).unwrap().read()
+        }
+    }
+
+    // don't do this:
+    // use std::ops::Deref;
+    // impl<I: Input, Z: Default + Deref<Target = I>> Input for Z {
+    //     fn read(&mut self) -> Result<u8, ReadError> {
+    //         self.read()
+    //     }
+    // }
+}
