f3aa9e3f2423c776def888bbef07744aaecab372
rrbutani <rr.butani@gmail.com>
2019-11-01 11:05:44 -0500

    shims: update the OutputShim

--- shims/src/peripherals/output.rs
+++ shims/src/peripherals/output.rs
@@ -1,86 +1,84 @@
-use lc3_traits::peripherals::output::{Output, WriteError};
-use std::io::{Write, stdout};
-use std::cell::RefCell;
+use lc3_traits::peripherals::output::{Output, OutputError};
+use std::io::{stdout, Error as IoError, Write};
+
 use std::rc::Rc;
 
-pub struct Shim {
-    sink: Rc<RefCell<dyn Write>>
+pub struct OutputShim<'a> {
+    sink: &'a mut dyn Write,
 }
 
-impl Default for Shim {
+impl Default for OutputShim<'_> {
     fn default() -> Self {
+        let out = Box::new(stdout());
+
         Self {
-            sink: Rc::new(RefCell::new(stdout()))
+            sink: std::boxed::Box::<std::io::Stdout>::leak(out), // TODO: DO NOT DO THIS!!!
         }
     }
 }
 
-impl Shim {
+impl<'a> OutputShim<'a> {
     fn new() -> Self {
         Self::default()
     }
-    
-    fn using(sink: Rc<RefCell<dyn Write>>) -> Self {
+
+    fn using(sink: &'a mut dyn Write) -> Self {
         Self { sink }
     }
 }
 
-impl Output for Shim {
-    fn write(&mut self, c: u8) -> Result<(), WriteError> {
-        let ret = match self.sink.borrow_mut().write(&[c]) {
-            Ok(1) => Ok(()), 
-            _ => Err(WriteError),
-        };
-        match self.sink.borrow_mut().flush() {
-            Ok(()) => ret,
-            _ => Err(WriteError)
-        }
+impl Output for OutputShim<'_> {
+    fn write(&mut self, c: u8) -> Result<(), OutputError> {
+        self.sink.write(&[c]).map_err(|_| OutputError)?;
+        self.sink.flush().map_err(|_| OutputError)?;
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn write_one() {
-        let sink = Rc::new(RefCell::new(Vec::new()));
-        let mut shim = Shim::using(sink.clone());
+        let mut sink = Vec::new();
+        let mut shim = OutputShim::using(&mut sink);
         let ch0 = 'A' as u8;
         let res = shim.write(ch0);
         assert_eq!(res, Ok(()));
-        assert_eq!(sink.borrow()[0], ch0);
+        assert_eq!(sink[0], ch0);
     }
 
     #[test]
     fn write_multiple() {
-        let sink = Rc::new(RefCell::new(Vec::new()));
-        let mut shim = Shim::using(sink.clone());
+        let mut sink = Vec::new();
+        let mut shim = OutputShim::using(&mut sink);
         let ch0 = 'L' as u8;
         let ch1 = 'C' as u8;
         let ch2 = '-' as u8;
         let ch3 = '3' as u8;
-        shim.write(ch0);
-        shim.write(ch1);
-        shim.write(ch2);
-        shim.write(ch3);
-        assert_eq!(sink.borrow()[0], ch0);
-        assert_eq!(sink.borrow()[1], ch1);
-        assert_eq!(sink.borrow()[2], ch2);
-        assert_eq!(sink.borrow()[3], ch3);
+        shim.write(ch0).unwrap();
+        shim.write(ch1).unwrap();
+        shim.write(ch2).unwrap();
+        shim.write(ch3).unwrap();
+        assert_eq!(sink[0], ch0);
+        assert_eq!(sink[1], ch1);
+        assert_eq!(sink[2], ch2);
+        assert_eq!(sink[3], ch3);
     }
-    
-    // Making a fixed-length Write is harder than it seems.
-//    #[test]
-//    fn write_too_much() {
-//        let mut buf: [u8; 1] = [0];
-//        let sink = Rc::new(RefCell::new(buf));
-//        let mut shim = Shim::using(sink.clone());
-//        let ch0 = 'Y' as u8;
-//        let ch1 = 'P' as u8;
-//        shim.write(ch0);
-//        let res = shim.write(ch1);
-//        assert_eq!(res, Err(WriteError));
-//    }
-}
 
+    #[test]
+    #[ignore]
+    // Annoyingly, this does not fail.
+    fn write_too_much() {
+        let mut buf: [u8; 1] = [0];
+        let sink = &mut buf.as_mut();
+        let mut shim = OutputShim::using(sink);
+        let ch0 = 'Y' as u8;
+        let ch1 = 'P' as u8;
+        shim.write(ch0).unwrap();
+        let res = shim.write(ch1);
+        // assert_eq!(res, Err(OutputError));
+    }
+}
