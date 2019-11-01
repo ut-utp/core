a7d8ab46040acf2e7cc39b8aee8bdcdab73ae792
rrbutani <rr.butani@gmail.com>
2019-11-01 11:05:14 -0500

    shims: misc refactoring on the InputShim

--- shims/src/peripherals/input.rs
+++ shims/src/peripherals/input.rs
@@ -1,29 +1,29 @@
 use lc3_traits::peripherals::input::{Input, ReadError};
 use std::io::{stdin, Read};
 
-pub struct Shim {
-    source: Box<dyn Read>
+pub struct InputShim {
+    source: Box<dyn Read>,
 }
 
-impl Default for Shim {
+impl Default for InputShim {
     fn default() -> Self {
         Self {
-            source: Box::new(stdin())
+            source: Box::new(stdin()),
         }
     }
 }
 
-impl Shim {
+impl InputShim {
     fn new() -> Self {
         Self::default()
     }
-    
+
     fn using(source: Box<dyn Read>) -> Self {
         Self { source }
     }
 }
 
-impl Input for Shim {
+impl Input for InputShim {
     fn read(&mut self) -> Result<u8, ReadError> {
         let mut buf: [u8; 1] = [0];
         match self.source.read(&mut buf) {
@@ -37,27 +37,27 @@ impl Input for Shim {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn read_nothing() {
         let source: Box<dyn Read> = Box::new("".as_bytes());
-        let mut shim = Shim::using(source);
+        let mut shim = InputShim::using(source);
         let res = shim.read();
         assert_eq!(res, Err(ReadError));
     }
-    
+
     #[test]
     fn reads_one() {
         let source: Box<dyn Read> = Box::new("A".as_bytes());
-        let mut shim = Shim::using(source);
+        let mut shim = InputShim::using(source);
         let res = shim.read();
         assert_eq!(res, Ok('A' as u8));
     }
-    
+
     #[test]
     fn reads_first() {
         let source: Box<dyn Read> = Box::new("Hello, world!".as_bytes());
-        let mut shim = Shim::using(source);
+        let mut shim = InputShim::using(source);
         let res = shim.read();
         assert_eq!(res, Ok('H' as u8));
     }
@@ -65,7 +65,7 @@ mod tests {
     #[test]
     fn reads_multiple() {
         let source: Box<dyn Read> = Box::new("Hello, world!".as_bytes());
-        let mut shim = Shim::using(source);
+        let mut shim = InputShim::using(source);
         let mut res = shim.read();
         assert_eq!(res, Ok('H' as u8));
         res = shim.read();
@@ -75,15 +75,14 @@ mod tests {
         res = shim.read();
         assert_eq!(res, Ok('l' as u8));
     }
-    
+
     #[test]
     fn read_too_many() {
         let source: Box<dyn Read> = Box::new("Hi".as_bytes());
-        let mut shim = Shim::using(source);
+        let mut shim = InputShim::using(source);
         shim.read();
         shim.read();
         let res = shim.read();
         assert_eq!(res, Err(ReadError));
     }
-    
 }
