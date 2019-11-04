4ea8ccb8907d93b6ae7b28eed76fc684e21eb195
rrbutani <rr.butani@gmail.com>
2019-11-04 08:32:18 -0600

    isa: fmt

--- isa/src/misc.rs
+++ isa/src/misc.rs
@@ -2,7 +2,6 @@
 //!
 //! (TODO!)
 
-
 /// Formally, the input and output devices that are part of the LC-3 only
 /// support ASCII characters. We'd like to support more than just ASCII; ideally
 /// Unicode text in general but therein lies a problem.
@@ -74,30 +73,29 @@
 ///
 ///    The above fails! It complains with:
 ///
-///   ```text
-///    stderr:
-///    thread 'main' panicked at 'assertion failed: `(left == right)`
-///      left: `"A"`,
-///     right: `"\u{0}A"`
-///   ```
+///    ```text
+///     stderr:
+///     thread 'main' panicked at 'assertion failed: `(left == right)`
+///       left: `"A"`,
+///      right: `"\u{0}A"`
+///    ```
 ///
-///   So what's going on? As mentioned, the issue is that we're always sending
-///   two bytes along to be processed as characters.
+///    So what's going on? As mentioned, the issue is that we're always sending
+///    two bytes along to be processed as characters.
 ///
-///   `(0x41).to_be_bytes()` gives us back `[0x00, 0x41]`; `0x00` gets processed
-///   as it's own character and produces the `"\u{0}"` we see above.
+///    `(0x41).to_be_bytes()` gives us back `[0x00, 0x41]`; `0x00` gets processed
+///    as it's own character and produces the `"\u{0}"` we see above.
 ///
-///   This is definitely something we could work around. We could add some kind
-///   of special termination character that Unicode reserves that denotes that
-///   we're sending a one byte character. Or, since we know that we're only
-///   looking for one character, we could just start from the back and stop once
-///   we've got our character.
+///    This is definitely something we could work around. We could add some kind
+///    of special termination character that Unicode reserves that denotes that
+///    we're sending a one byte character. Or, since we know that we're only
+///    looking for one character, we could just start from the back and stop once
+///    we've got our character.
 ///
-///   Here's an implementation of the latter:
+///    Here's an implementation of the latter:
 ///
-///   ```rust
-///   # use lc3_isa::Word;
-///
+///    ```rust
+///    # use lc3_isa::Word;
 ///    fn encode<'a>(string: &'a str) -> impl Iterator<Item = Word> + 'a {
 ///        string.chars().map(|c| {
 ///            match c.len_utf8() {
@@ -130,82 +128,82 @@
 ///        string
 ///    }
 ///
-///   let orig = "Ԋəllo World¡";
-///   assert_eq!(orig, decode(encode(orig)));
-///   ```
+///    let orig = "Ԋəllo World¡";
+///    assert_eq!(orig, decode(encode(orig)));
+///    ```
 ///
-///   As expected, this panics on things it can't represent in two bytes:
+///    As expected, this panics on things it can't represent in two bytes:
 ///
-///   ```rust,should_panic
-///   # use lc3_isa::Word;
+///    ```rust,should_panic
+///    # use lc3_isa::Word;
+///    #
+///    # fn encode<'a>(string: &'a str) -> impl Iterator<Item = Word> + 'a {
+///    #     string.chars().map(|c| {
+///    #         match c.len_utf8() {
+///    #             3..=4 => panic!("Can't represent `{}` in <=2 bytes!", c),
+///    #             2 => {
+///    #                 let mut buf: [u8; 2] = [0, 0];
+///    #                 c.encode_utf8(&mut buf);
+///    #                 u16::from_be_bytes(buf)
+///    #             },
+///    #             1 => {
+///    #                 let mut buf: [u8; 1] = [0];
+///    #                 c.encode_utf8(&mut buf);
+///    #                 u16::from_le_bytes([buf[0], 0])
+///    #             },
+///    #             _ => unreachable!(),
+///    #         }
+///    #     })
+///    # }
+///    #
+///    # fn decode(iter: impl Iterator<Item = Word>) -> String {
+///    #     let mut string = String::new();
+///    #
+///    #     iter.for_each(|c| {
+///    #         let b = c.to_be_bytes();
+///    #         let s = std::str::from_utf8(&b).unwrap();
+///    #
+///    #         string.push(s.chars().nth_back(0).unwrap());
+///    #     });
+///    #
+///    #     string
+///    # }
+///    #
+///    assert_eq!("🎉", decode(encode("🎉")));
+///    ```
 ///
-///   # fn encode<'a>(string: &'a str) -> impl Iterator<Item = Word> + 'a {
-///   #     string.chars().map(|c| {
-///   #         match c.len_utf8() {
-///   #             3..=4 => panic!("Can't represent `{}` in <=2 bytes!", c),
-///   #             2 => {
-///   #                 let mut buf: [u8; 2] = [0, 0];
-///   #                 c.encode_utf8(&mut buf);
-///   #                 u16::from_be_bytes(buf)
-///   #             },
-///   #             1 => {
-///   #                 let mut buf: [u8; 1] = [0];
-///   #                 c.encode_utf8(&mut buf);
-///   #                 u16::from_le_bytes([buf[0], 0])
-///   #             },
-///   #             _ => unreachable!(),
-///   #         }
-///   #     })
-///   # }
+///    So clearly, this is an _option_, but it's a bit hacky. Luckily, there's a
+///    better way.
 ///
-///   # fn decode(iter: impl Iterator<Item = Word>) -> String {
-///   #     let mut string = String::new();
-///   #
-///   #     iter.for_each(|c| {
-///   #         let b = c.to_be_bytes();
-///   #         let s = std::str::from_utf8(&b).unwrap();
-///   #
-///   #         string.push(s.chars().nth_back(0).unwrap());
-///   #     });
-///   #
-///   #     string
-///   # }
+///    As mentioned, UTF-8 is but one Unicode encoding. Another is UTF-16:
+///    similar to UTF-8 it's a variable length encoding but with 16 bit units
+///    instead of bytes. Encoded characters can occupy two or four bytes.
 ///
-///   assert_eq!("🎉", decode(encode("🎉")));
-///   ```
+///    This solves the problem we tried to solve above. To abide by our
+///    DDR/KBDR/R0 size limitations we'd have to limit ourselves to the
+///    characters that can be encoded in two bytes. It turns out this is actually
+///    an (old) encoding of it's own: UCS-2. It's since fallen out of use since
+///    it can't represent all the Unicode codepoints in existence today but if
+///    we're going to limit ourselves to two bytes anyways this isn't a concern.
 ///
-///   So clearly, this is an _option_, but it's a bit hacky. Luckily, there's a
-///   better way.
+///    So, to recap, if we're okay with slightly tweaking the TRAPs and using
+///    some unused bits in registers (but not modifying their sizes) we can gain
+///    access to some of the Unicode codepoints *and* retain backwards
+///    compatibility with the existing calling conventions and such.
 ///
-///   As mentioned, UTF-8 is but one Unicode encoding. Another is UTF-16:
-///   similar to UTF-8 it's a variable length encoding but with 16 bit units
-///   instead of bytes. Encoded characters can occupy two or four bytes.
+///    Additionally, if we go with the hacky UTF-8 based scheme in the code
+///    above we can make PUTSP (and it's assembler directive counterpart) Unicode
+///    aware (unpack two 1 byte characters stored in a word; send 2 byte chars in
+///    one go; probably don't splice 2 byte chars across words).
 ///
-///   This solves the problem we tried to solve above. To abide by our
-///   DDR/KBDR/R0 size limitations we'd have to limit ourselves to the
-///   characters that can be encoded in two bytes. It turns out this is actually
-///   an (old) encoding of it's own: UCS-2. It's since fallen out of use since
-///   it can't represent all the Unicode codepoints in existence today but if
-///   we're going to limit ourselves to two bytes anyways this isn't a concern.
+///    From a backwards compatibility standpoint this is basically perfect. As an
+///    added bonus no additional complexity is foist upon users unless they want
+///    the additional functionality.
 ///
-///   So, to recap, if we're okay with slightly tweaking the TRAPs and using
-///   some unused bits in registers (but not modifying their sizes) we can gain
-///   access to some of the Unicode codepoints *and* retain backwards
-///   compatibility with the existing calling conventions and such.
-///
-///   Additionally, if we go with the hacky UTF-8 based scheme in the code
-///   above we can make PUTSP (and it's assembler directive counterpart) Unicode
-///   aware (unpack two 1 byte characters stored in a word; send 2 byte chars in
-///   one go; probably don't splice 2 byte chars across words).
-///
-///   From a backwards compatibility standpoint this is basically perfect. As an
-///   added bonus no additional complexity is foist upon users unless they want
-///   the additional functionality.
-///
-///   The only real downside is that this doesn't provide full Unicode support.
-///   Supporting other languages is really the point of this (the emojis are
-///   nice too though) and, for example, CJK codepoints require 3 bytes in
-///   UTF-8. So:
+///    The only real downside is that this doesn't provide full Unicode support.
+///    Supporting other languages is really the point of this (the emojis are
+///    nice too though) and, for example, CJK codepoints require 3 bytes in
+///    UTF-8. So:
 ///
 /// 3) What if we went further?
 pub fn __() -> () {}
