0cef8264b56cc7d2558f7ab498705cdd11277864
Rahul Butani <rr.butani@gmail.com>
2019-11-30 14:17:00 -0800

    macros: some nice, but ultimately unusable, macros
    ((can't use proc macros in statement or expression positions yet))

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -42,10 +42,62 @@
 #![doc(test(attr(deny(rust_2018_idioms, warnings))))]
 #![doc(html_logo_url = "")] // TODO!
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate proc_macro;
+use proc_macro2::Span;
+use quote::quote_spanned;
+
+/// Report an error with the given `span` and message.
+pub(crate) fn spanned_err(span: Span, msg: impl Into<String>) -> proc_macro::TokenStream {
+    let msg = msg.into();
+    quote_spanned!(span.into() => {
+        compile_error!(#msg);
+    })
+    .into()
+}
+
+// pub mod lifetime_to_identifier;
+
+// pub use lifetime_to_identifier::lifetime_to_ident;
+
+use proc_macro::TokenTree;
+use quote::quote;
+use syn::{parse::Parse, parse::ParseStream, parse::Result as ParseResult, Ident};
+use syn::{parse_macro_input, Lifetime};
+
+/// Takes a lifetime and turns it into an identifier.
+#[proc_macro]
+pub fn lifetime_to_ident(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(tokens as Lifetime);
+
+    proc_macro::TokenStream::from(quote!(#input.ident))
+}
+
+struct LifetimeVarDecl {
+    ident: Ident,
+    rest: proc_macro2::TokenTree,
+}
+
+impl Parse for LifetimeVarDecl {
+    fn parse(input: ParseStream<'_>) -> ParseResult<Self> {
+        let lifetime = input.parse::<Lifetime>()?;
+
+        let tree = input.cursor().token_tree().ok_or_else(|| {
+            syn::Error::new(Span::call_site(), "Expected a lifetime and a token tree.")
+        })?;
+
+        Ok(LifetimeVarDecl {
+            ident: lifetime.ident,
+            rest: tree.0,
+        })
     }
+}
+
+#[proc_macro]
+pub fn create_label(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(tokens as LifetimeVarDecl);
+
+    let ident = input.ident;
+    let tree = input.rest;
+
+    proc_macro::TokenStream::from(quote!(let #ident: Addr = #tree;))
 }
