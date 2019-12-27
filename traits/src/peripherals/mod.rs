647a3e2a74ed12f859053880a631f6f647b3379b
Rahul Butani <rr.butani@gmail.com>
2019-12-27 14:39:47 -0800

    traits: some experiments with generalizing over wrapper types

--- traits/src/peripherals/mod.rs
+++ traits/src/peripherals/mod.rs
@@ -19,6 +19,7 @@ pub use input::Input;
 pub use output::Output;
 
 use core::marker::PhantomData;
+// use core::ops::{Deref, DerefMut};
 
 pub trait Peripherals<'p>:
     Gpio<'p> + Adc + Pwm + Timers<'p> + Clock + Input<'p> + Output<'p>
@@ -26,7 +27,20 @@ pub trait Peripherals<'p>:
     fn init(&mut self);
 }
 
-pub struct PeripheralSet<'p, G, A, P, T, C, I, O>
+// pub trait DerefOrOwned<RealType>: sealed::Sealed<RealType> { }
+
+// mod sealed {
+//     use core::ops::{Deref, DerefMut};
+//     pub trait Sealed<R> { }
+
+//     impl<T> Sealed<T> for T { }
+//     // impl<P, T> Sealed<T> for P where P: Deref<Target = T> + DerefMut { }
+// }
+
+// impl<T> DerefOrOwned<T> for T { }
+// impl<P, T> DerefOrOwned<T> for P where P: Deref<Target = T> + DerefMut { }
+
+pub struct PeripheralSet<'p, G, A, P, T, C, I, O/*, GW, AW, PW, TW, CW, IW, OW*/>
 where
     G: Gpio<'p>,
     A: Adc,
@@ -35,7 +49,21 @@ where
     C: Clock,
     I: Input<'p>,
     O: Output<'p>,
+    // GW: 'p + DerefOrOwned<G>,
+    // AW: 'p + DerefOrOwned<A>,
+    // PW: 'p + DerefOrOwned<P>,
+    // TW: 'p + DerefOrOwned<T>,
+    // CW: 'p + DerefOrOwned<C>,
+    // IW: 'p + DerefOrOwned<I>,
+    // OW: 'p + DerefOrOwned<O>,
 {
+    // gpio: GW,
+    // adc: AW,
+    // pwm: PW,
+    // timers: TW,
+    // clock: CW,
+    // input: IW,
+    // output: OW,
     gpio: G,
     adc: A,
     pwm: P,
@@ -49,7 +77,7 @@ where
 // TODO: is default a supertrait requirement or just an additional bound here
 // (as in, if all your things implement default, we'll give you a default
 // otherwise no).
-impl<'p, G, A, P, T, C, I, O> Default for PeripheralSet<'p, G, A, P, T, C, I, O>
+impl<'p, G, A, P, T, C, I, O> Default for PeripheralSet<'p, G, A, P, T, C, I, O/*, G, A, P, T, C, I, O*/>
 where
     G: Gpio<'p>,
     A: Adc,
@@ -73,7 +101,7 @@ where
     }
 }
 
-impl<'p, G, A, P, T, C, I, O> PeripheralSet<'p, G, A, P, T, C, I, O>
+impl<'p, G, A, P, T, C, I, O/*, GW, AW, PW, TW, CW, IW, OW*/> PeripheralSet<'p, G, A, P, T, C, I, O/*, GW, AW, PW, TW, CW, IW, OW*/>
 where
     G: Gpio<'p>,
     A: Adc,
@@ -82,6 +110,13 @@ where
     C: Clock,
     I: Input<'p>,
     O: Output<'p>,
+    // GW: 'p + DerefOrOwned<G>,
+    // AW: 'p + DerefOrOwned<A>,
+    // PW: 'p + DerefOrOwned<P>,
+    // TW: 'p + DerefOrOwned<T>,
+    // CW: 'p + DerefOrOwned<C>,
+    // IW: 'p + DerefOrOwned<I>,
+    // OW: 'p + DerefOrOwned<O>,
 {
     pub fn new(gpio: G, adc: A, pwm: P, timers: T, clock: C, input: I, output: O) -> Self {
         Self {
@@ -125,6 +160,40 @@ where
     }
 }
 
+// enum WrapperType {
+//     Dual,
+//     Mutless,
+// }
+
+// // TODO: type to lock on each access that implements
+// trait RunInContext<Wrapped> {
+//     const TYPE: WrapperType;
+
+//     fn with_ref<R, F: FnOnce(&Wrapped) -> R>(&self, func: F) -> R;
+//     fn with_mut<R, F: FnOnce(&mut Wrapped) -> R>(&mut self, func: F) -> R;
+// }
+
+// impl<T, P: AsRef<T> + AsMut<T>> RunInContext<T> for P {
+//     const TYPE: WrapperType = WrapperType::Dual;
+
+//     fn with_ref<R, F: FnOnce(&T) -> R>(&self, func: F) -> R { func(self.as_ref()) }
+//     fn with_mut<R, F: FnOnce(&mut T) -> R>(&mut self, func: F) -> R { func(self.as_mut()) }
+// }
+
+// impl<T, I: RunInContext<T>> RunInContext<T> for Arc<I> {
+//     fn with_ref<R, F: FnOnce(&T) -> R>(&self, func: F) -> R {
+//         func()
+//     }
+
+//     fn with_mut<R, F: FnOnce(&T) -> R>(&mut self, func: F) -> F {
+
+//     }
+// }
+
+// struct LockOnAccess<T>(T);
+
+// struct BorrowOnAccess<T>(T);
+
 #[doc(hidden)]
 #[macro_export]
 macro_rules! peripheral_trait {
@@ -132,7 +201,34 @@ macro_rules! peripheral_trait {
         $(#[doc = $doc])*
         pub trait $trait $(<$lifetime>)? where Self: $($bound)? { $($rest)* }
 
-        $crate::peripheral_set_impl!($trait<$($lifetime)?> $(| $lifetime |)?, { $crate::func_sig!($nom, $($rest)*); });
+        // $crate::deref_impl!($trait$(<$lifetime>)? $(| $lifetime |)?, { $($rest)* });
+        // $crate::borrow_impl!($trait$(<$lifetime>)? $(| $lifetime |)?, { $($rest)* });
+        $crate::peripheral_set_impl!($trait$(<$lifetime>)? $(| $lifetime |)?, { $crate::func_sig!($nom, $($rest)*); });
+        // $crate::peripheral_deref_set_impl!($trait$(<$lifetime>)? $(| $lifetime |)?, { $crate::func_sig!($nom, $($rest)*); });
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! deref_impl {
+    ($trait:path $(| $lifetime:lifetime |)?, { $($rest:tt)* }) => {
+        #[allow(unnecessary_qualification)]
+        impl<$($lifetime,)? I, T: Default + core::ops::Deref<Target = I> + core::ops::DerefMut> $trait for T
+        where
+            I: $trait,
+        { $crate::func_sig!(+(*), $($rest)*); }
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! borrow_impl {
+    ($trait:path $(| $lifetime:lifetime |)?, { $($rest:tt)* }) => {
+        #[allow(unnecessary_qualification)]
+        impl<$($lifetime,)? I, T: Default + core::ops::Deref<Target = I> + core::ops::DerefMut + core::borrow::Borrow<I> + core::borrow::BorrowMut<I>> $trait for T
+        where
+            I: $trait,
+        { $crate::func_sig!(%(borrow, borrow_mut), $($rest)*); }
     };
 }
 
@@ -140,7 +236,7 @@ macro_rules! peripheral_trait {
 #[macro_export]
 macro_rules! peripheral_set_impl {
     ($trait:ty $(| $lifetime:lifetime |)?, { $($rest:tt)* }) => {
-        impl<$($lifetime,)? 'p, G, A, P, T, C, I, O> $trait for $crate::peripherals::PeripheralSet<'p, G, A, P, T, C, I, O>
+        impl<$($lifetime,)? 'p, G, A, P, T, C, I, O> $trait for $crate::peripherals::PeripheralSet<'p, G, A, P, T, C, I, O/*, G, A, P, T, C, I, O*/>
         where
             $($lifetime: 'p,)?
             G: 'p + $crate::peripherals::gpio::Gpio<'p>,
@@ -154,96 +250,115 @@ macro_rules! peripheral_set_impl {
     };
 }
 
+// #[doc(hidden)]
+// #[macro_export]
+// macro_rules! peripheral_deref_set_impl {
+//     ($trait:ty $(| $lifetime:lifetime |)?, { $($rest:tt)* }) => {
+//         impl<$($lifetime,)? 'p, G, A, P, T, C, I, O, GInner, AInner, PInner, TInner, CInner, IInner, OInner> $trait for $crate::peripherals::PeripheralSet<'p, G, A, P, T, C, I, O>
+//         where
+//             $($lifetime: 'p,)?
+//             G: 'p + $crate::peripherals::gpio::Gpio<'p>,
+//             A: 'p + $crate::peripherals::adc::Adc,
+//             P: 'p + $crate::peripherals::pwm::Pwm,
+//             T: 'p + $crate::peripherals::timers::Timers<'p>,
+//             C: 'p + $crate::peripherals::clock::Clock,
+//             I: 'p + $crate::peripherals::input::Input<'p>,
+//             O: 'p + $crate::peripherals::output::Output<'p>,
+//             GInner: 'p + Deref<
+//         { $($rest)* }
+//     };
+// }
+
 #[doc(hidden)]
 #[macro_export]
 macro_rules! func_sig {
     // [No block + Ret] Our ideal form: specified return type, no block:
     // (none)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
+    ($(+($indir:tt))? $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
         fn $fn_name($($idents : $types),*) -> $ret { compile_error!("trait functions not supported yet!") }
-        $crate::func_sig!($nom, $($rest)*);
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, $($rest)*);
     };
     // (self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
-        fn $fn_name(self, $($idents : $types),*) -> $ret { self.$nom.$fn_name($($idents),*) }
-        $crate::func_sig!($nom, $($rest)*);
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
+        fn $fn_name(self, $($idents : $types),*) -> $ret { ($($indir$indir)?self)$(.$nom)?$(.$i_im())?.$fn_name($($idents),*) }
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, $($rest)*);
     };
     // (mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
-        fn $fn_name(mut self, $($idents : $types),*) -> $ret { self.$nom.$fn_name($($idents),*) }
-        $crate::func_sig!($nom, $($rest)*);
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
+        fn $fn_name(mut self, $($idents : $types),*) -> $ret { ($($indir$indir)?self)$(.$nom)?$(.$i_mut())?.$fn_name($($idents),*) }
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, $($rest)*);
     };
     // (&self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
-        fn $fn_name(&self, $($idents : $types),*) -> $ret { self.$nom.$fn_name($($idents),*) }
-        $crate::func_sig!($nom, $($rest)*);
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
+        fn $fn_name(&self, $($idents : $types),*) -> $ret { ($($indir$indir)?self)$(.$nom)?$(.$i_im())?.$fn_name($($idents),*) }
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, $($rest)*);
     };
     // (&mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
-        fn $fn_name(&mut self, $($idents : $types),*) -> $ret { self.$nom.$fn_name($($idents),*) }
-        $crate::func_sig!($nom, $($rest)*);
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self$(,)? $($idents:ident : $types:ty),*) -> $ret:ty; $($rest:tt)*) => {
+        fn $fn_name(&mut self, $($idents : $types),*) -> $ret { ($($indir$indir)?self)$(.$nom)?$(.$i_mut())?.$fn_name($($idents),*) }
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, $($rest)*);
     };
 
 
     // [Block + Ret] Ditch blocks if you've got them:
     // (none)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name($($idents : $types),*) -> $ret; $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name($($idents : $types),*) -> $ret; $($rest)*); };
     // (self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($self:expr,)? $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(self, $($idents : $types),*) -> $ret; $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($self:expr,)? $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(self, $($idents : $types),*) -> $ret; $($rest)*); };
     // (mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(mut self, $($idents : $types),*) -> $ret; $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(mut self, $($idents : $types),*) -> $ret; $($rest)*); };
     // (&self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&self $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&self, $($idents : $types),*) -> $ret; $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&self $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&self, $($idents : $types),*) -> $ret; $($rest)*); };
     // (&mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&mut self, $($idents : $types),*) -> $ret; $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*) -> $ret:ty $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&mut self, $($idents : $types),*) -> $ret; $($rest)*); };
 
 
     // [No Block + No Ret] Add in return types if they're not specified:
     // (none)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*); $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name($($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident($($idents:ident : $types:ty),*); $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name($($idents : $types),*) -> (); $($rest)*); };
     // (self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(self, $($idents : $types),*) -> (); $($rest)*); };
     // (mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(mut self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(mut self, $($idents : $types),*) -> (); $($rest)*); };
     // (&self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&self, $($idents : $types),*) -> (); $($rest)*); };
     // (&mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&mut self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*); $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&mut self, $($idents : $types),*) -> (); $($rest)*); };
 
 
     // [Block + No Ret] Strip blocks + add in return types:
     // (none)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident( $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name($($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident( $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name($($idents : $types),*) -> (); $($rest)*); };
     // (self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(self, $($idents : $types),*) -> (); $($rest)*); };
     // (mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(mut self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(mut self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(mut self, $($idents : $types),*) -> (); $($rest)*); };
     // (&self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&self $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&self $($idents : $types),*) -> (); $($rest)*); };
     // (&mut self)
-    ($nom:ident, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
-        $crate::func_sig!($nom, fn $fn_name(&mut self, $($idents : $types),*) -> (); $($rest)*); };
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, $(#[doc = $doc:expr])* fn $fn_name:ident(&mut self, $($idents:ident : $types:ty),*) $block:block $($rest:tt)*) => {
+        $crate::func_sig!($(+($indir))? $(%($i_im, $i_mut))? $($nom)?, fn $fn_name(&mut self, $($idents : $types),*) -> (); $($rest)*); };
 
 
     // And, finally, the end:
-    ($nom:ident, ) => {};
+    ($(+($indir:tt))?  $(%($i_im:ident, $i_mut:ident))? $($nom:ident)?, ) => {};
 }
 
-impl<'p, G, A, P, T, C, I, O> Peripherals<'p> for PeripheralSet<'p, G, A, P, T, C, I, O>
+impl<'p, G, A, P, T, C, I, O> Peripherals<'p> for PeripheralSet<'p, G, A, P, T, C, I, O/*, G, A, P, T, C, I, O*/>
 where
     G: 'p + Gpio<'p>,
     A: 'p + Adc,
