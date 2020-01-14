49fb73d14a92bd1fc582b9793f978c138258b189
Rahul Butani <rr.butani@gmail.com>
2020-01-13 16:15:05 -0800

    control: add support for chaining encodings together

--- traits/src/control/rpc.rs
+++ traits/src/control/rpc.rs
@@ -168,6 +168,93 @@ impl<T: Debug> Encoding<T> for TransparentEncoding<T> {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+// Outer provides:
+//   - A -> Outer::Encoded
+//   - Outer::Encoded -> A
+//
+// Inner provides:
+//   - B -> Inner::Encoded
+//   - Inner::Encoded -> B
+//
+// Assuming Outer::Encoded = B, we have a full cycle:
+//   [A] -> [B / Outer::Encoded] -> [Inner::Encoded]
+//    ^                                    |
+//    \- <- [Outer::Encoded / B] <--------/
+//
+// The above contains two encodes and two decodes; this type squashes the above
+// into one encode and one decode:
+//   [A] -> [Inner::Encoded]
+//    ^            |
+//     \----------/
+pub struct ChainedEncoding<A: Debug, B: Debug, Outer: Encoding<A>, Inner: Encoding<B>>(PhantomData<Outer>, PhantomData<Inner>)
+where
+    Outer: Encoding<A, Encoded = B>,
+    <Inner as Encoding<B>>::Err: Into<<Outer as Encoding<A>>::Err>;
+
+
+impl<A, B, Outer, Inner> Encoding<A> for ChainedEncoding<A, B, Outer, Inner>
+where
+    A: Debug,
+    B: Debug,
+    Outer: Encoding<A, Encoded = B>,
+    Inner: Encoding<B>,
+    <Inner as Encoding<B>>::Err: Into<<Outer as Encoding<A>>::Err>
+{
+    type Encoded = <Inner as Encoding<B>>::Encoded;
+    type Err = <Outer as Encoding<A>>::Err;
+
+    fn encode(message: A) -> Self::Encoded {
+        let b: B = <Outer as Encoding<A>>::encode(message);
+        <Inner as Encoding<B>>::encode(b)
+    }
+
+    fn decode(message: &Self::Encoded) -> Result<A, Self::Err> {
+        let b: B = <Inner as Encoding<B>>::decode(message)?;
+        <Outer as Encoding<A>>::decode(b)
+    }
+}
+
+impl<A, Outer> ChainedEncoding<A, <Outer as Encoding<A>>::Encoded, Outer, TransparentEncoding<<Outer as Encoding<A>>::Encoded>>
+where
+    A: Debug,
+    Outer: Encoding<A>,
+    // !: Into<<Outer as Encoding<A>>::Err>,
+{
+    pub fn new_detached() -> Self {
+        Default::default()
+    }
+
+    pub fn new(_outer: Outer) -> Self {
+        Self::new_detached()
+    }
+}
+
+
+impl<A, B, Outer, Inner> ChainedEncoding<A, B, Outer, Inner>
+where
+    A: Debug,
+    B: Debug,
+    Outer: Encoding<A, Encoded = B>,
+    Inner: Encoding<B>,
+    <Inner as Encoding<B>>::Err: Into<<Outer as Encoding<A>>::Err>
+{
+    pub fn chain_detatched<Z: Debug, NewOuter: Encoding<Z, Encoded = A>>() -> ChainedEncoding<Z, A, NewOuter, Self>
+    where
+        <Outer as Encoding<A>>::Err: Into<<NewOuter as Encoding<Z>>::Err>
+    {
+        Default::default()
+    }
+
+    pub fn chain<Z: Debug, NewOuter: Encoding<Z, Encoded = A>>(self, _new_outer: NewOuter) -> ChainedEncoding<Z, A, NewOuter, Self>
+    where
+        <Outer as Encoding<A>>::Err: Into<<NewOuter as Encoding<Z>>::Err>
+    {
+        Self::chain_detatched()
+    }
+
+}
+
 pub trait Transport<EncodedFormat> {
     type Err: Debug;
 
