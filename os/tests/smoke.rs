dd4029bbd01ef32d5a0cd1aa4cea4f434742fba6
Rahul Butani <rr.butani@gmail.com>
2020-03-11 06:56:30 -0500

    os: os is big!

--- os/tests/smoke.rs
+++ os/tests/smoke.rs
@@ -9,6 +9,5 @@ use common::with_larger_stack;
 
 #[test]
 fn os_size() {
-    // TODO: update
-    with_larger_stack(|| assert_eq!(OS.into_iter().count(), 0x03B4));
+    with_larger_stack(|| assert_eq!(OS.into_iter().count(), 0x04F9));
 }
