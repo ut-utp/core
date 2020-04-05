76f846a9df9dcb1e897285ab485e6e07971481f1
Rahul Butani <rr.butani@gmail.com>
2020-04-05 18:50:10 -0500

    os: update the smoke and trap tests to use the test-infrastructure crate

--- os/tests/smoke.rs
+++ os/tests/smoke.rs
@@ -1,15 +1,10 @@
 //! Just check that the OS assembles.
 
+extern crate lc3_test_infrastructure as lti;
+
 use lc3_os::*;
 
-use pretty_assertions::assert_eq;
-
-// Note: same as the trap tests; this is brittle and kind of bad; at some point
-// it'd be good to spin off the shared testing infrastructure crate that lives
-// in this workspace (TODO, low priority).
-#[path = "../../baseline-sim/tests/test_infrastructure/mod.rs"]
-mod common;
-use common::with_larger_stack;
+use lti::{assert_eq, with_larger_stack};
 
 #[test]
 fn os_size() {
