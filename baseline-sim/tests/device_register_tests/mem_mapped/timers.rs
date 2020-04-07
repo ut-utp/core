e2cbc8787f112de1fe00c669d886d7da2bbc2325
David Gipson <david.gipson@utexas.edu>
2020-04-07 15:43:12 -0500

    baseline-sim: add flaky repeated timer test

--- baseline-sim/tests/device_register_tests/mem_mapped/timers.rs
+++ baseline-sim/tests/device_register_tests/mem_mapped/timers.rs
@@ -46,3 +46,40 @@ single_test! {
     regs: { },
     memory: { }
 }
+
+// TODO: flaky (sometimes hangs)
+single_test! {
+    repeated_100ms,
+    prefill: {
+        0x3010: 0x0700,
+        0x3011: T0CR_ADDR,
+        0x3012: T0DR_ADDR,
+        0x3013: 100,
+        0x3014: <MCR as MemMapped>::ADDR,
+        0x3015: 0xFFFC, // -4
+    },
+    prefill_expr: {
+        (TIMER_BASE_INT_VEC): 0x3009,
+        (<PSR as MemMapped>::ADDR): 0x0302,
+    },
+    insns: [
+        { LD R6, #0xF },    // Set nonzero R6
+        { AND R0, R0, #0 },
+        { ADD R0, R0, #1 }, // Mode: repeated
+        { STI R0, #0xD },   // Set to repeated
+        { LD R0, #0xE },    // Load period (100ms)
+        { STI R0, #0xC },   // Set period to 100ms
+        { LD R0, #0xE },    // Check if interrupt fired five times
+        { BRnz #-2 },       // Go back one if not
+
+        { AND R0, R0, #0 }, // Prep HALT
+        { STI R0, #0xA },   // HALT (0x3009)
+
+        { LD R1, #0xA },
+        { ADD R1, R1, #1 },
+        { ST R1, #9 },
+        { RTI } // 0x300D
+    ],
+    regs: { },
+    memory: { }
+}
