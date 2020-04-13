790bc16359e37ebe9cf0b57b9d6e81863366194e
jer_zhang <jer_zhang@yahoo.com>
2020-04-13 15:08:38 -0500

    os: fix inclusivity bug in bounds checking

--- os/src/os.rs
+++ os/src/os.rs
@@ -1010,7 +1010,7 @@ fn os() -> AssembledProgram {
         // R0 = value to check
         // R4 = max value
         // -> cc = n if out of bounds
-        //         p if within bounds
+        //         z or p if within bounds
         // Does not modify R0
         // Destroys R1
         @CHECK_OUT_OF_BOUNDS
@@ -1019,8 +1019,8 @@ fn os() -> AssembledProgram {
             NOT R4, R4;                     // Negate R4
             ADD R4, R4, #1;
             ADD R4, R0, R4;                 // Check if R0 is less than R4
-            BRp @OUT_OF_BOUNDS;
-            ADD R0, R0, #0;                 // If not, set cc to p
+            BRzp @OUT_OF_BOUNDS;
+            ADD R0, R0, #0;                 // If not, set cc to z or p
             BR @OUT_OF_BOUNDS_RET;
         @OUT_OF_BOUNDS
             NOT R4, R0;                     // Set cc to n
