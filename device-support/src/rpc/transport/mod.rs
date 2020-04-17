e11b1c922420e4455dd6853e2e38ce365d44855f
Rahul Butani <rr.butani@gmail.com>
2020-04-17 14:13:07 -0500

    dev-support: a baseline uart based transport impl

--- device-support/src/rpc/transport/mod.rs
+++ device-support/src/rpc/transport/mod.rs
@@ -6,8 +6,9 @@ use lc3_traits::control::rpc::{RequestMessage, ResponseMessage};
 
 use core::mem::size_of;
 
-
 // Check that CAPACITY is such that we can hold at least one full
 // request/response:
 sa::const_assert!(fifo::CAPACITY >= (3 * size_of::<RequestMessage>()));
 sa::const_assert!(fifo::CAPACITY >= (3 * size_of::<ResponseMessage>()));
+
+pub mod uart_simple;
