783657cdfe88946920b6fa5114055b4c50e04452
Rahul Butani <rr.butani@gmail.com>
2020-04-17 00:47:40 -0500

    test-infra: fix the lc3_sequence macro

--- test-infrastructure/src/lc3tools/macros.rs
+++ test-infrastructure/src/lc3tools/macros.rs
@@ -1,15 +1,18 @@
 //! Home of the `lc3_sequence!` macro.
 
+#[macro_export]
 macro_rules! lc3_sequence {
     (
         $(|$panics:literal|)?
         $name:ident,
-        insns: [ $({ $($insn:tt)* }),* ],
+        insns: [ $({ $($insn:tt)* }),* $(,)?],
     ) => {
         $(#[doc = $panics] #[should_panic])?
         #[test]
-        fn $name() { $crate::with_larger_stack(None, || {
-            use $crate::{Instruction, PeripheralInterruptFlags, MemoryShim, PeripheralsShim};
+        fn $name() -> std::io::Result<()> { $crate::with_larger_stack(None, || {
+            use $crate::{
+                Instruction, PeripheralInterruptFlags, MemoryShim, PeripheralsShim, insn
+            };
 
             #[allow(unused_mut)]
             let mut insns: Vec<Instruction> = Vec::new();
@@ -20,8 +23,8 @@ macro_rules! lc3_sequence {
             $crate::lc3tools::lc3tools_tester::<MemoryShim, PeripheralsShim, _, _>(
                 insns,
                 &flags,
-                &None
-            );
+                None
+            )
         })}
     };
 }
