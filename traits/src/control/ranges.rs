befefc5446096194ef0a6ef2a18bc1c22a69b3cd
Rahul Butani <rr.butani@gmail.com>
2020-04-21 14:43:40 -0500

    control: update UnifiedRange to be serializable

--- traits/src/control/ranges.rs
+++ traits/src/control/ranges.rs
@@ -5,58 +5,77 @@
 
 use core::ops::{
     Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
-    Bound, RangeBounds
+    Bound as CoreBound, RangeBounds
 };
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum UnifiedRange<T> {
-    Range(Range<T>),
-    RangeFrom(RangeFrom<T>),
-    RangeFull(RangeFull),
-    RangeInclusive(RangeInclusive<T>),
-    RangeTo(RangeTo<T>),
-    RangeToInclusive(RangeToInclusive<T>),
+pub struct UnifiedRange<T> {
+    start: Bound<T>,
+    end: Bound<T>,
+}
+
+// A copy of `core::ops::Bound` that we can serialize (and turn into
+// `core::ops::Bound` instances).
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+enum Bound<T> {
+    Includes(T),
+    Excludes(T),
+    Unbounded
+}
+
+impl<T: Clone> From<CoreBound<&T>> for Bound<T> {
+    #[inline]
+    fn from(b: CoreBound<&T>) -> Self {
+        match b {
+            CoreBound::Included(b) => Bound::Includes(b.clone()),
+            CoreBound::Excluded(b) => Bound::Excludes(b.clone()),
+            CoreBound::Unbounded => Bound::Unbounded,
+        }
+    }
+}
+
+impl<T> Bound<T> {
+    #[inline]
+    fn as_core_bound(&self) -> CoreBound<&T> {
+        match self {
+            Bound::Includes(b) => CoreBound::Included(b),
+            Bound::Excludes(b) => CoreBound::Excluded(b),
+            Bound::Unbounded => CoreBound::Unbounded,
+        }
+    }
 }
 
 impl<T> RangeBounds<T> for UnifiedRange<T> {
-    fn start_bound(&self) -> Bound<&T> {
-        match self {
-            Self::Range(r) => r.start_bound(),
-            Self::RangeFrom(r) => r.start_bound(),
-            Self::RangeFull(r) => r.start_bound(),
-            Self::RangeInclusive(r) => r.start_bound(),
-            Self::RangeTo(r) => r.start_bound(),
-            Self::RangeToInclusive(r) => r.start_bound(),
-        }
+    fn start_bound(&self) -> CoreBound<&T> {
+        self.start.as_core_bound()
     }
 
-    fn end_bound(&self) -> Bound<&T> {
-        match self {
-            Self::Range(r) => r.end_bound(),
-            Self::RangeFrom(r) => r.end_bound(),
-            Self::RangeFull(r) => r.end_bound(),
-            Self::RangeInclusive(r) => r.end_bound(),
-            Self::RangeTo(r) => r.end_bound(),
-            Self::RangeToInclusive(r) => r.end_bound(),
-        }
+    fn end_bound(&self) -> CoreBound<&T> {
+        self.end.as_core_bound()
     }
 }
 
 macro_rules! into {
     ($($ty:tt)*) => {$(
-        impl<T> From<$ty::<T>> for UnifiedRange<T> {
+        impl<T: Clone> From<$ty::<T>> for UnifiedRange<T> {
             fn from(r: $ty<T>) -> Self {
-                UnifiedRange::$ty(r)
+                UnifiedRange {
+                    start: r.start_bound().into(),
+                    end: r.end_bound().into(),
+                }
             }
         }
     )*};
 }
 
-impl<T> From<RangeFull> for UnifiedRange<T> {
+impl<T: Clone> From<RangeFull> for UnifiedRange<T> {
     fn from(r: RangeFull) -> Self {
-        Self::RangeFull(r)
+        Self {
+            start: r.start_bound().into(),
+            end: r.end_bound().into(),
+        }
     }
 }
 
