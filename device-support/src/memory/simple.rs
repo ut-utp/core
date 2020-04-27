aad288d8147f24823bf2921b269ef3a449c31909
Rahul Butani <rr.butani@gmail.com>
2020-04-27 01:01:05 -0500

    dev-support: change the ram backed memory impl once more

--- device-support/src/memory/simple.rs
+++ device-support/src/memory/simple.rs
@@ -42,23 +42,23 @@ use lc3_traits::control::load::Index as IndexWrapper;
 ///  58: 0x3A00 - 0x3AFF :: backed (user prog)
 ///  59: 0x3B00 - 0x3BFF :: backed (user prog)
 ///  60: 0x3C00 - 0x3CFF :: backed (user prog)
-///  61: 0x3D00 - 0x3DFF :: backed (user prog)
-///  62: 0x3E00 - 0x3EFF :: backed (user prog)
-///  63: 0x3F00 - 0x3FFF :: backed (user prog)
-///  64: 0x4000 - 0x40FF :: backed (user prog)
+// ///  61: 0x3D00 - 0x3DFF :: backed (user prog)
+// ///  62: 0x3E00 - 0x3EFF :: backed (user prog)
+// ///  63: 0x3F00 - 0x3FFF :: backed (user prog)
+// ///  64: 0x4000 - 0x40FF :: backed (user prog)
 // ///  65: 0x4100 - 0x41FF :: backed (user prog)
 // ///  66: 0x4200 - 0x42FF :: backed (user prog)
 /// ..........................................
 /// 254: 0xFE00 - 0xFEFF :: backed (mem mapped special)
 /// 255: 0xFF00 - 0xFFFF :: backed (mem mapped special)
 ///
-/// 28 of these pages will occupy 14KiB of RAM, which we should be able to
+/// 24 of these pages will occupy 12KiB of RAM, which we should be able to
 /// handle.
 ///
 #[repr(packed)]
 pub struct PartialMemory {
     program_data: ProgramMetadata,
-    pages: [[Word; Self::PAGE_SIZE]; 28],
+    pages: [[Word; Self::PAGE_SIZE]; 24],
     zero: Word,
     void: Word,
 }
@@ -67,10 +67,10 @@ static __PARTIAL_MEM_SIZE_CHK: () = {
     let canary = [()];
     let size = core::mem::size_of::<PartialMemory>();
 
-    canary[size - ((28 * 512) + 8 + 28)]
+    canary[size - ((24 * 512) + 8 + 28)]
 };
 
-sa::const_assert!(core::mem::size_of::<PartialMemory>() == (28 * 512) + 8 + 28);
+sa::const_assert!(core::mem::size_of::<PartialMemory>() == (24 * 512) + 8 + 28);
 
 impl PartialMemory {
     const PAGE_SIZE: usize = 0x0100; // TODO: Use `PageAccess`?
@@ -81,8 +81,8 @@ impl PartialMemory {
 
         match addr {
             0x0000..=0x07FF => Some(((addr as usize / Self::PAGE_SIZE), offset)),
-            0x2F00..=0x40FF => Some(((addr as usize / Self::PAGE_SIZE) - 0x2F + 8, offset)),
-            0xFE00..=0xFFFF => Some(((addr as usize / Self::PAGE_SIZE) - 0xFE + 26, offset)),
+            0x2F00..=0x3CFF => Some(((addr as usize / Self::PAGE_SIZE) - 0x2F + 8, offset)),
+            0xFE00..=0xFFFF => Some(((addr as usize / Self::PAGE_SIZE) - 0xFE + 22, offset)),
             _ => None,
         }
     }
@@ -127,7 +127,7 @@ impl PartialMemory {
     #[inline]
     pub const fn new() -> Self {
         Self {
-            pages: [[0; PartialMemory::PAGE_SIZE]; 28],
+            pages: [[0; PartialMemory::PAGE_SIZE]; 24],
             program_data: ProgramMetadata::empty(),
             zero: 0,
             void: 0,
